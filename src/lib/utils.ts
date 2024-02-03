b91565434a3e2096e8c05c50b61f07e3a7874129
Kishor U <upadhyaya13kishor@gmail.com>
2024-02-03 11:15:00 +0545

    Add utility functions for managing tokens

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function setToken(token: string) {
+  localStorage.setItem("token", token);
+}
+
+export function getToken() {
+  return localStorage.getItem("token");
+}
+
+export function clearToken() {
+  localStorage.clear();
 }
