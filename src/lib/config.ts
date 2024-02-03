d3e46ada3b3e9c5ecf10afb4cb648de8a0fe1052
Kishor U <upadhyaya13kishor@gmail.com>
2024-02-03 11:15:55 +0545

    Add backend client configuration with interceptors

---
+++ src/lib/config.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const backendClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL!
+});
+
+backendClient.interceptors.request.use(async function (config) {
+  try {
+    const token = localStorage.getItem("token");
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`
+    } as any;
+  } catch (error) {
+    console.error("Error obtaining auth token in interceptor, ", error);
+  }
+  return config;
+});
+
+backendClient.interceptors.response.use(
+  (response) => Promise.resolve(response),
+  async (error) => {
+    if (!error.response) {
+      return Promise.reject(error);
+    }
+    if (error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = `/login?expired=true`;
+    } else {
+      return Promise.resolve(error.response);
+    }
+  }
+);
+
+export { backendClient };
