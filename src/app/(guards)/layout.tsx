7bdfc09262b5c7f26cf07ea5f074a54b9da21487
Kishor U <upadhyaya13kishor@gmail.com>
2024-02-03 11:15:27 +0545

    Refactor layout components and add Toaster component

--- src/app/(guards)/layout.tsx
+++ src/app/(guards)/layout.tsx
@@ -1,12 +1,28 @@
+"use client";
 import React from "react";
 import { Nav } from "@/components/nav";
+import { Toaster } from "@/components/ui/toaster";
+import { getToken } from "@/lib/utils";
+import { redirect } from "next/navigation";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const checkIfLoggedIn = () => {
+    const token = getToken();
+    if (!token) {
+      return false;
+    }
+    return true;
+  };
+  if (!checkIfLoggedIn()) {
+    return redirect("/");
+  }
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Nav />
-      <main
-      className="max-w-[1300px] mx-auto"
-      >{children}</main>
-    </>
+      <Toaster />
+      <main className="max-w-[1300px] mx-auto">{children}</main>
+    </QueryClientProvider>
   );
 }
--- src/app/(outer)/layout.tsx
+++ src/app/(outer)/layout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Nav } from "@/components/nav-outer";
+import { Toaster } from "@/components/ui/toaster";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Nav />
-      <main
-      className="max-w-[1300px] mx-auto"
-      >{children}</main>
+      <Toaster />
+      <main className="max-w-[1300px] mx-auto">{children}</main>
     </>
   );
 }
